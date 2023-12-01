2a54a6951ec52e5cacf325fa1baf6a116ac4768a
sigreyo <simongrenefalk@hotmail.com>
2023-12-01 14:25:30 +0100

    day 1 part 2

--- go/1/1.go
+++ go/1/1.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -22,18 +24,20 @@ func main() {
 
 	sum := 0
 	for scanner.Scan() {
-		lineWithNumbers := []rune(scanner.Text())
-		numbers := []int{}
-		for _, char := range lineWithNumbers {
-			if unicode.IsDigit(char) {
-				number, _ := strconv.Atoi(string(char))
-				numbers = append(numbers, number)
-			}
-		}
+		lineWithNumbers := scanner.Text()
+
+		// numbers := []int{}
+		// for _, char := range lineWithNumbers {
+		// 	if unicode.IsDigit(char) {
+		// 		number, _ := strconv.Atoi(string(char))
+		// 		numbers = append(numbers, number)
+		// 	}
+		// }
+
+		numbers := checkForWordedNumbers(lineWithNumbers)
 
 		lineSum, _ := addFirstAndLast(numbers[0], numbers[len(numbers)-1])
 		sum += lineSum
-
 	}
 
 	fmt.Println(sum)
@@ -41,5 +45,35 @@ func main() {
 
 func addFirstAndLast(first, last int) (int, error) {
 	return strconv.Atoi(strconv.Itoa(first) + strconv.Itoa(last))
+}
 
+func checkForWordedNumbers(line string) []int {
+	wordedNumbers := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	lineNumbers := []int{}
+
+	for index, char := range line {
+		if unicode.IsDigit(char) {
+			value, _ := strconv.Atoi(string(char))
+			lineNumbers = append(lineNumbers, value)
+		} else {
+			for _, word := range wordedNumbers {
+				if strings.Contains(line, word) {
+
+					for _, letter := range word {
+						if char == letter {
+							if len(word)+index > len(line) {
+								break
+							}
+							if line[index:(index+len(word))] == word {
+								lineNumbers = append(lineNumbers, slices.Index(wordedNumbers, word)+1)
+							}
+						} else {
+							break
+						}
+					}
+				}
+			}
+		}
+	}
+	return lineNumbers
 }
